c532a38ba4d2136332ac40be7e2a051ddb04c8ec
Vincent Migot <vincent.migot@inra.fr>
2019-02-12 14:54:17 +0100

    Update index.php for opensilex-test

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <html>
     <head>
         <title>PHIS</title>
-        <meta http-equiv="refresh" content="0;URL=http://138.102.159.37/mtpvm/web">
+        <meta http-equiv="refresh" content="0;URL=http://138.102.159.37/opensilex-test/web">
     </head>
     <body></body>
 </html>

fba02e5947c12e27ac31a0ea107dfbede1ae3063
Pierre-Etienne Alary <pierre-etienne.alary@inra.fr>
2019-05-07 18:12:33 +0200

    fix incorrect display on the reflectance file insert

--- views/radiometric-target/_form.php
+++ views/radiometric-target/_form.php
@@ -157,13 +157,11 @@ use kartik\file\FileInput;
                 <td><p><?= Yii::t('app/messages', 'Values in nanometers (450 nm, 560 nm, ...) of the wavelengths at which the radiometric target reflectance has been measured (real numbers)') ?> </p> </td>
             </tr>
              <tr class="dataset-variables">
-                <th style="color:red"><?=Yii::t('app', 'Reflectance value') * ?></th>
+                <th style="color:red"><?=Yii::t('app', 'Reflectance value (between 0 and 1)') * ?></th>
                 <td ><?= Yii::t('app/messages', 'Values of the spectral hemispherical reflectance in wavelength of the radiometric target for each of the wavelength listed in the first row (real numbers included in a range of [0 - 1])') ?></td>
             </tr>
-            <tr>
-                <th><p><?= Yii::t('app/messages', 'The spectral hemispherical reflectance file uses tabluations (\t) as field separators and dots (.) as decimal separators.') ?> </p> </th>
-            </tr>
         </table>
+        <p><?= Yii::t('app/messages', 'The spectral hemispherical reflectance file uses tabluations (\t) as field separators and dots (.) as decimal separators.') ?> </p>
     </div>
 
 
